ebb2d3d1d5d35785cb595ce23788ffba08fe776b
amite <amit.erandole@gmail.com>
2018-03-04 17:24:54 +0530

    creates detached version

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "expb00c16ec349f49828b5dda9ef8029ae4://8d-x4b.amite.shaadi-lite-expo.exp.direct:80";
+
+}
